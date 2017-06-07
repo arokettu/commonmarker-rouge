1e14fac7ec6997fecc88d0625c372f9fd181f1b1
Steve Smith <steve@orderedlist.com>
2017-06-07 11:41:01 -0400

    Add spec for escaping language fence

--- spec/commonmarker_rouge_spec.rb
+++ spec/commonmarker_rouge_spec.rb
@@ -16,4 +16,19 @@ MD
 
     expect(html).to_not eq('')
   end
+
+  it 'escapes language markup' do
+    html = CommonMarker::Rouge.render_html(<<-MD)
+```haxxor">yay!
+somecode
+```
+MD
+
+    expected = <<-HTML
+<div class="highlighter-rouge language-haxxor&quot;&gt;yay!"><pre class=\"highlight\"><code>somecode
+</code></pre>
+</div>
+HTML
+    expect(html).to eq(expected)
+  end
 end
