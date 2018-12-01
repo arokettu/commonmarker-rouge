d1eee2ff6907597fa4d85870e4df91259d60fba9
Anton Smirnov <sandfox@sandfox.me>
2018-12-01 04:50:28 +0200

    Forgot about the block support :(

--- spec/commonmarker_rouge_spec.rb
+++ spec/commonmarker_rouge_spec.rb
@@ -2,73 +2,73 @@ require 'spec_helper'
 
 describe CommonMarker::Rouge do
   it 'does something useful' do
-    html = CommonMarker::Rouge.render_html(<<~MD)
-        # Test
-        
-        ```
-        somecode
-        ```
-        
-        ```ruby
-        puts 'highlight as ruby'
-        ```
-    MD
+    html = CommonMarker::Rouge.render_html(<<-MD)
+# Test
 
-    expected = <<~HTML
-        <h1>Test</h1>
-        <pre><code>somecode
-        </code></pre>
-        <div class="highlighter-rouge language-ruby"><span class="nb">puts</span> <span class="s1">'highlight as ruby'</span>
-        </div>
-    HTML
+```
+somecode
+```
+
+```ruby
+puts 'highlight as ruby'
+```
+MD
+
+    expected = <<-HTML
+<h1>Test</h1>
+<pre><code>somecode
+</code></pre>
+<div class="highlighter-rouge language-ruby"><span class="nb">puts</span> <span class="s1">'highlight as ruby'</span>
+</div>
+HTML
 
     expect(html).to eq(expected)
   end
 
   it 'escapes language markup' do
-    html = CommonMarker::Rouge.render_html(<<~MD)
-        ```haxxor">yay!
-        somecode
-        ```
-    MD
+    html = CommonMarker::Rouge.render_html(<<-MD)
+```haxxor">yay!
+somecode
+```
+MD
 
     expected = 'language-haxxor&quot;&gt;yay!'
     expect(html).to include(expected) # find only escaped sequence
   end
 
   it 'supports table extensions' do
-    html = CommonMarker::Rouge.render_html(<<~MD, [:DEFAULT], [:DEFAULT], [:table])
-        | foo | bar |
-        | --- | --- |
-        | baz | bim |
-    MD
+    html = CommonMarker::Rouge.render_html(<<-MD, [:DEFAULT], [:DEFAULT], [:table])
+| foo | bar |
+| --- | --- |
+| baz | bim |
+MD
 
-    expected = <<~HTML
-        <table>
-        <thead>
-        <tr>
-        <th>foo</th>
-        <th>bar</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-        <td>baz</td>
-        <td>bim</td>
-        </tr>
-        </tbody>
-        </table>
-    HTML
+    expected = <<-HTML
+<table>
+<thead>
+<tr>
+<th>foo</th>
+<th>bar</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>baz</td>
+<td>bim</td>
+</tr>
+</tbody>
+</table>
+HTML
 
     expect(html).to(eq(expected))
   end
 
   it 'does not support table extensions' do
-    html = CommonMarker::Rouge.render_html(<<~MD, [:DEFAULT])
-        | foo | bar |
-        | --- | --- |
-        | baz | bim |
-    MD
+    html = CommonMarker::Rouge.render_html(<<-MD, [:DEFAULT])
+| foo | bar |
+| --- | --- |
+| baz | bim |
+MD
 
     expected = "<p>| foo | bar |\n| --- | --- |\n| baz | bim |</p>\n"
 
